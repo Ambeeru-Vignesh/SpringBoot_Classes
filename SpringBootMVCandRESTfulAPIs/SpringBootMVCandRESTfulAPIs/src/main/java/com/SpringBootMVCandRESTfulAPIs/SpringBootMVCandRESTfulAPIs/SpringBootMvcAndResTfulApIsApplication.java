3e8cc7cb4ef6493783b249acf9e5d0a5e10b95a1
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-15 21:09:12 +0530

    initial commit

---
+++ SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/src/main/java/com/SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/SpringBootMvcAndResTfulApIsApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcAndResTfulApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcAndResTfulApIsApplication.class, args);
+	}
+
+}
---
+++ SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/src/test/java/com/SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/SpringBootMvcAndResTfulApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcAndResTfulApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
