22f7006d066686d1e95462592a0b73a80ad21e8a
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-18 13:22:40 +0530

    input validation and custom validators

--- SpringBoot MVC and RESTful APIs/SpringBootMVCandRESTfulAPIs/src/main/java/com/SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/controllers/EmployeeController.java
+++ SpringBoot MVC and RESTful APIs/SpringBootMVCandRESTfulAPIs/src/main/java/com/SpringBootMVCandRESTfulAPIs/SpringBootMVCandRESTfulAPIs/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs.dto.EmployeeD
 import com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs.entities.EmployeeEntity;
 import com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs.repositories.EmployeeRepository;
 import com.SpringBootMVCandRESTfulAPIs.SpringBootMVCandRESTfulAPIs.services.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -39,13 +40,13 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+    public ResponseEntity<EmployeeDTO> createNewEmployee(@RequestBody @Valid EmployeeDTO inputEmployee){
         EmployeeDTO savedEmployee = employeeService.createNewEmployee(inputEmployee);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @PutMapping(path = "/{employeeID}")
-    public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody EmployeeDTO employeeDTO, @PathVariable Long employeeID){
+    public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody @Valid EmployeeDTO employeeDTO, @PathVariable Long employeeID){
         return ResponseEntity.ok(employeeService.updateEmployeeID(employeeDTO, employeeID));
     }
 
