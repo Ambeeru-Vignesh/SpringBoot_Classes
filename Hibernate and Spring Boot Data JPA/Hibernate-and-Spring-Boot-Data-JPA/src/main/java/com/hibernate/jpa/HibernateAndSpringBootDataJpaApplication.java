3abcb2bb32caf796c2fcc1ef945a1dd554c18a5a
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-21 15:20:35 +0530

    initial commit

---
+++ Hibernate and Spring Boot Data JPA/Hibernate-and-Spring-Boot-Data-JPA/src/main/java/com/hibernate/jpa/HibernateAndSpringBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAndSpringBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAndSpringBootDataJpaApplication.class, args);
+	}
+
+}
