809fc02ebfd2ef617c8f751a7c35574ace2331f5
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-04 21:45:16 +0530

    initial commit

---
+++ Production-ready-Spring-Boot-Features/src/test/java/com/production_ready_feature/ProductionReadySpringBootFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.production_ready_feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionReadySpringBootFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
