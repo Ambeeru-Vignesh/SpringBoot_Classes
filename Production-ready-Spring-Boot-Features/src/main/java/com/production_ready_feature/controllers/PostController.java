05cd7ed8c41ad477929ce1919d901da968901520
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-07 22:44:06 +0530

    post controller

--- Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/controllers/PostController.java
+++ Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/controllers/PostController.java
@@ -1,4 +1,38 @@
 package com.production_ready_feature.controllers;
 
+import com.production_ready_feature.dto.PostDTO;
+import com.production_ready_feature.services.PostService;
+import jdk.dynalink.linker.LinkerServices;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.internal.bytebuddy.asm.Advice;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public List<PostDTO> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{postId")
+    public PostDTO getPostById(@PathVariable Long postId) {
+        return postService.getPostById(postId);
+    }
+
+    @PostMapping
+    public PostDTO createNewPost(@RequestBody PostDTO inputPost) {
+        return postService.createNewPost(inputPost);
+    }
+
+    @PostMapping("{postId}")
+    public PostDTO updatePost(@RequestBody PostDTO inputPost, @PathVariable Long postId) {
+        return postService.updatePost(inputPost, postId);
+    }
 }
