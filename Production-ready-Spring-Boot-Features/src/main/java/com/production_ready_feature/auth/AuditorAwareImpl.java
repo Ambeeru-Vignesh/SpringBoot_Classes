b0f7bd1db00a6218cdcb50465bb6e317952e7c86
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-08-08 18:12:53 +0530

    audit config

--- Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/auth/AuditorAwareImpl.java
+++ Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/auth/AuditorAwareImpl.java
@@ -1,4 +1,14 @@
 package com.production_ready_feature.auth;
 
-public class AuditorAwareImpl {
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Vignesh Ambeeru");
+    }
 }
--- Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/config/AppConfig.java
+++ Production-ready-Spring-Boot-Features/src/main/java/com/production_ready_feature/config/AppConfig.java
@@ -1,4 +1,25 @@
 package com.production_ready_feature.config;
 
+import com.production_ready_feature.auth.AuditorAwareImpl;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "getAuditorAwareImpl")
 public class AppConfig {
+
+    @Bean
+    ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    AuditorAware<String> getAuditorAwareImpl() {
+        return new AuditorAwareImpl();
+    }
 }
