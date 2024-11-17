d7f3fb74a295418936dbaa2a9bb271fd247a762e
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-11-17 11:00:34 +0530

    service test prototype

--- Testing-spring-boot/src/test/java/Testing/spring/boot/services/impl/EmployeeServiceImplTest.java
+++ Testing-spring-boot/src/test/java/Testing/spring/boot/services/impl/EmployeeServiceImplTest.java
@@ -1,14 +1,49 @@
 package Testing.spring.boot.services.impl;
 
+import Testing.spring.boot.TestcontainersConfiguration;
+import Testing.spring.boot.dto.EmployeeDto;
+import Testing.spring.boot.entities.Employee;
+import Testing.spring.boot.repositories.EmployeeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(TestcontainersConfiguration.class)
+@ExtendWith(MockitoExtension.class)
 class EmployeeServiceImplTest {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @Spy
+    private ModelMapper modelMapper;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    private Employee mockEmployee;
+    private EmployeeDto mockEmployeeDto;
 
     @BeforeEach
     void setUp() {
+        mockEmployee = Employee.builder()
+                .id(1L)
+                .email("vignesh@gmail.com")
+                .name("Vignesh")
+                .salary(400L)
+                .build();
+
+        mockEmployeeDto = modelMapper.map(mockEmployee, EmployeeDto.class);
     }
 
     @Test
