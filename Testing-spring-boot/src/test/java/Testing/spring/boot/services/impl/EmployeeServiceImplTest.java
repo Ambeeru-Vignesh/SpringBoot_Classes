9ad4a8bf25c30fe0f6e6db18d7d8d57cda5cbdf8
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-11-17 10:49:05 +0530

    service test prototype

---
+++ Testing-spring-boot/src/test/java/Testing/spring/boot/services/impl/EmployeeServiceImplTest.java
@@ -0,0 +1,29 @@
+package Testing.spring.boot.services.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void createNewEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
